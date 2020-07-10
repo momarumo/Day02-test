fe4c01193231efbfe9178a572dcdb50f4fb87517
Mohau Marumo <momarumo@c5r6s7.wethinkcode.co.za>
2020-07-10 10:24:23 +0200

    printalphabet

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: momarumo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/10 09:50:02 by momarumo          #+#    #+#             */
+/*   Updated: 2020/07/10 10:22:25 by momarumo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char letter;
+	letter = 'a';
+	while(letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+}
+
+
